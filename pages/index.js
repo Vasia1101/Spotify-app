78bc8203b1f9cbd57107cda4e63db58502f92be5
Vasia1101 <vasilij.gajda@gmail.com>
2022-01-17 16:19:30 +0200

    add hero icon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-// import Sidebar from '../components/Sidebar'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       <h1>Vasyl Haida app</h1>
       <main className="bg-black h-screen overflow-hidden">
-        {/* <Sidebar /> */}
+        <Sidebar />
         Center Block
       </main>
       <div>
